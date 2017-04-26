de808240f618d7b86c708dceb3b887f0bac0dcc7
JeanBilheux <bilheuxjm@ornl.gov>
2017-04-26 08:37:13 -0400

    Finished the implementation and added the unit tests

--- sectorizedradialprofile/calculate_radial_profile.py
+++ sectorizedradialprofile/calculate_radial_profile.py
@@ -29,7 +29,6 @@ class CalculateRadialProfile(object):
             except:
                 raise ValueError
 
-
         if angle_range:
             try:
                 from_angle = angle_range['from']
@@ -38,3 +37,104 @@ class CalculateRadialProfile(object):
                 self.to_angle = to_angle
             except:
                 raise ValueError
+
+    def calculate(self):
+        '''performs the radial profile calculation'''
+        
+        if self.data == []:
+            return
+        
+        self.calculate_array_size()
+        #self.convert_angles_to_radians()
+        self.calculate_pixels_radius()
+        self.calculate_pixels_angle_position()
+        self.turn_off_data_outside_angle_range()
+        self.sort_indices_of_radius()
+        self.sort_radius()
+        self.sort_data_by_radius_value()
+        self.calculate_radius_bins_location()
+        self.calculate_radius_bins_size()
+        self.calculate_profile()
+        
+    def calculate_profile(self):
+        '''calculate the final profile'''
+
+        # cumulative sum to figure out total counts for each radii bin
+        cumulative_sum = np.cumsum(self.data_sorted_by_radius, dtype=np.float64)
+        
+        # sum of counts for each radii bin
+        total_counts_bin = cumulative_sum[self.radius_bins_location[1:]] - \
+            cumulative_sum[self.radius_bins_location[:-1]]
+
+        # calculate profile
+        self.radial_profile = total_counts_bin / self.radius_bins_size
+        
+    def calculate_radius_bins_size(self):
+        '''define the size of each radius bin size'''
+        self.radius_bins_size = self.radius_bins_location[1:] - self.radius_bins_location[:-1]
+        
+    def calculate_radius_bins_location(self):
+        '''define an array of indices that show the integer radius jumps'''
+        _integer_radius = self.sorted_radius.astype(np.int32)
+        _delta_r = _integer_radius[1:] - _integer_radius[:-1]
+        self.radius_bins_location = np.where(_delta_r)[0]
+        
+    def sort_data_by_radius_value(self):
+        '''sort the working data by radius indices'''
+        self.data_sorted_by_radius = self.working_data.flat[self.sorted_radius_indices]
+        
+    def sort_radius(self):
+        '''sort the radius array'''
+        self.sorted_radius = self.radius_array.flat[self.sorted_radius_indices]
+        
+    def sort_indices_of_radius(self):
+        '''sort the indices of the radius array'''
+        sort_indices = np.argsort(self.radius_array.flat)
+        self.sorted_radius_indices = sort_indices
+        
+    def turn_off_data_outside_angle_range(self):
+        '''using the angle range provided and the angle value of each pixels,
+        this algorithm replace all the initial data by 0 outside the range specified'''
+        left_angles_indices = self.array_angle_deg >= self.from_angle
+        right_angles_indices = self.array_angle_deg <= self.to_angle
+        
+        angles_to_keep = np.logical_and(left_angles_indices, right_angles_indices)
+        angles_to_not_keep = np.invert(angles_to_keep)
+        
+        working_data = np.array(self.data)
+        working_data[angles_to_not_keep] = 0
+        
+        self.working_data = working_data
+
+    def calculate_array_size(self):
+        '''retrieve the width and height of the array'''
+        [self.height, self.width] = np.shape(self.data)
+        
+    def calculate_pixels_angle_position(self):
+        '''determine the angle position related to the top vertical center of
+        each pixel in radians'''
+        complex_array = (self.height - self.y_index - self.y0) + 1j * \
+            (self.x_index - self.x0)
+        array_angle_deg = np.angle(complex_array, deg=True)
+        
+        #removing all negative angles -> [0, 360[
+        array_angle_deg_pos = np.array(array_angle_deg)
+        for _y in np.arange(self.height):
+            for _x in np.arange(self.width):
+                _value = array_angle_deg[_y, _x]
+                if _value < 0:
+                    array_angle_deg_pos[_y, _x] = 2 * 180 + _value
+
+        self.array_angle_deg = array_angle_deg_pos
+        
+    def calculate_pixels_radius(self):
+        '''calculate radii of all pixels '''
+        self.y_index, self.x_index = np.indices(self.data.shape)
+        radius = np.sqrt((self.x_index-self.x0)**2 + (self.y_index-self.y0)**2)      
+        self.radius_array = radius
+    
+    #def convert_angles_to_radians(self):
+        #'''convert from degress to radians the angles. This is necessary
+        #to evaluate the angle range with the the angle position of each pixel'''
+        #self.from_angle_rad = np.deg2rad(self.from_angle)
+        #self.to_angle_rad = np.deg2rad(self.to_angle)
--- tests/sectorizedradiaprofile/test_calculate_profile.py
+++ tests/sectorizedradiaprofile/test_calculate_profile.py
@@ -69,3 +69,214 @@ class TestClass(unittest.TestCase):
         bad_angle_range = {'from': 10,
                            'too': 20}
         self.assertRaises(ValueError, CalculateRadialProfile, [], {}, bad_angle_range)
+        
+    #def test_angle_conversion_to_rad(self):
+        #'''assert angle are correctly converted to radians'''
+        #data = np.array(Image.open(self.data_path))
+        #data = data[:, :, 1]
+    
+        #[height, width] = np.shape(data)
+        #[y0, x0] = [int(height/2), int(width/2)]
+        #center = {'x0': x0,
+                  #'y0': y0}
+        
+        #angle_range = {'from': 0,
+                       #'to': 90}
+        #o_calculate = CalculateRadialProfile(data=data,
+                                             #center=center,
+                                             #angle_range=angle_range)
+        #o_calculate.calculate()
+        
+        #assert 0 == o_calculate.from_angle_rad
+        #self.assertAlmostEqual(3.14/2, o_calculate.to_angle_rad, delta = 0.001)
+        
+    def test_calculation_of_radius_array(self):
+        '''assert that the array of pixel radius is correct'''
+        data = np.ones((10,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                 angle_range=angle_range)
+        o_calculate.calculate()
+        
+        _radius_array = o_calculate.radius_array
+        assert _radius_array[y0, x0] == 0   #center of circle
+        self.assertAlmostEquals(_radius_array[0,0], 7.071, delta=0.01)
+
+    def test_report_size_of_image(self):
+        '''assert the size of the array is correctly retrieved'''
+        data = np.ones((20,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                     angle_range=angle_range)
+        o_calculate.calculate_array_size()
+        assert 10 == o_calculate.width
+        assert 20 == o_calculate.height
+        
+    def test_calculate_array_of_angles(self):
+        '''assert the array of angles is correct'''
+        data = np.ones((10,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                     angle_range=angle_range)
+        o_calculate.calculate()
+        array_angle_deg = o_calculate.array_angle_deg
+        
+        assert array_angle_deg[0,0] == 315
+        assert array_angle_deg[y0, x0] == 0
+        assert array_angle_deg[height/2, 0] == 270
+        self.assertAlmostEqual(array_angle_deg[height-1, 0], 231.3, delta=0.1)
+        
+    def test_new_working_data(self):
+        '''assert the data outside the angle range are turned off - working data is correct'''
+        data = np.ones((10,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                     angle_range=angle_range)
+        o_calculate.calculate()
+        working_data = o_calculate.working_data
+        
+        real_working_data = np.zeros((10,10))
+        real_working_data[0:6, 5:,] = 1
+        
+        assert (working_data == real_working_data).all()
+        
+    def test_sort_indices_of_radius(self):
+        '''assert the array of radius indices is correctly sorted'''
+        data = np.ones((10,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                     angle_range=angle_range)
+        o_calculate.calculate()
+        sorted_radius_indices = o_calculate.sorted_radius_indices
+                
+        assert 55 == sorted_radius_indices[0]
+        assert 0 == sorted_radius_indices[-1]
+        
+    def test_sort_radius(self):
+        '''assert the array of radius is correctly sorted'''
+        data = np.ones((10,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                     angle_range=angle_range)
+        o_calculate.calculate()
+        sorted_radius = o_calculate.sorted_radius
+        
+        assert 0 == sorted_radius[0]
+        self.assertAlmostEqual(7.071, sorted_radius[-1], delta=0.01)
+        
+    def test_working_data_correctly_sorted(self):
+        '''assert the working data are correctly sorted according to the pixel radius'''
+        data = np.ones((10,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                     angle_range=angle_range)
+        o_calculate.calculate()
+        data_sorted_by_radius = o_calculate.data_sorted_by_radius
+        
+        assert ([1,1,0,1,0,0,1,0,0,1,1,0,0,1,0,1] == data_sorted_by_radius[:16]).all()
+        
+    def test_radius_bins_location(self):
+        '''assert the location of radius bins'''
+        data = np.ones((10,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                     angle_range=angle_range)
+        o_calculate.calculate()        
+        radius_bins_location = o_calculate.radius_bins_location
+        
+        assert ([0, 8, 24, 44, 68, 94, 98] == radius_bins_location).all()
+
+    def test_radius_bins_size(self):
+        '''assert the size of the radius bins'''
+        data = np.ones((10,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                     angle_range=angle_range)
+        o_calculate.calculate()        
+        radius_bins_size = o_calculate.radius_bins_size
+        
+        assert ([8, 16, 20, 24, 26, 4] == radius_bins_size).all()
+        
+    def test_profile(self):
+        '''assert the final profil'''
+        data = np.ones((10,10))
+        [height, width] = np.shape(data)
+        [y0, x0] = [int(height/2), int(width/2)]
+        center = {'x0': x0,
+                      'y0': y0}
+    
+        angle_range = {'from': 0,
+                           'to': 90}
+        o_calculate = CalculateRadialProfile(data=data,
+                                                 center=center,
+                                                     angle_range=angle_range)
+        o_calculate.calculate()        
+        radial_profile = o_calculate.radial_profile
+        
+        self.assertAlmostEquals(0.375, radial_profile[0], delta=0.01)
+        self.assertAlmostEqual(0.313, radial_profile[1], delta=0.01)
+        self.assertAlmostEqual(0.3, radial_profile[2], delta=0.01)
+        self.assertAlmostEqual(0.292, radial_profile[3], delta=0.01)
