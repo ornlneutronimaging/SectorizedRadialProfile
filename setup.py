34ad61d274467e7d99a97f8cf068c2c26cc0f046
Jean Bilheux <bilheuxjm@ornl.gov>
2017-04-28 11:22:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,7 @@ setup(
     author = "Jean Bilheux",
     author_email = "bilheuxjm@ornl.gov", 
     packages = find_packages(exclude=['tests', 'notebooks']),
-    #package_data = { 'sectorizedradialprofile' : ['data/material_list.dat', 
-                                           #'data/full_material_list.dat']},
     include_package_data = True,
-    #data_files = [('neutronbraggedge', ['neutronbraggedge/config.cfg']),
-                  #('neutronbraggedge/data', ['neutronbraggedge/data/material_list.dat'])
-                  #],
     test_suite = 'tests',
     install_requires = [
         'numpy',
@@ -30,7 +25,6 @@ setup(
                    'Intended Audience :: Developers',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3.5'],
-    # download_url = '',
 )
 
 
