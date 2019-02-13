4bbc7570c11fa9629dd1b29510a23b369b15a8a0
Yuxuan Zhang <zhangy6x@gmail.com>
2019-02-13 16:57:10 -0500

    updated 'setup.py' to limit to python 3

--- setup.py
+++ setup.py
@@ -2,31 +2,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "sectorizedradialprofile",
-    version = "1.0.0",
-    author = "Jean Bilheux",
-    author_email = "bilheuxjm@ornl.gov", 
-    packages = find_packages(exclude=['tests', 'notebooks']),
-    include_package_data = True,
-    test_suite = 'tests',
-    install_requires = [
+    name="sectorizedradialprofile",
+    version="1.0.0",
+    author="Jean Bilheux",
+    author_email="bilheuxjm@ornl.gov",
+    packages=find_packages(exclude=['tests', 'notebooks']),
+    include_package_data=True,
+    test_suite='tests',
+    install_requires=[
         'numpy',
         'pandas',
         'scikit-image',
     ],
-    dependency_links = [
+    dependency_links=[
     ],
-    description = "Radial profile of a given sector of an 2D array",
-    license = 'BSD',
-    keywords = "tiff tif profile radial sector",
-    url = "https://github.com/JeanBilheux/SectorizedRadialProfile",
-    classifiers = ['Development Status :: 3 - Alpha',
-                   'Topic :: Scientific/Engineering :: Physics',
-                   'Intended Audience :: Developers',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6'],
+    description="Radial profile of a given sector of an 2D array",
+    license='BSD',
+    keywords="tiff tif profile radial sector",
+    url="https://github.com/JeanBilheux/SectorizedRadialProfile",
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Intended Audience :: Developers',
+                 # 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6'],
 )
-
 
 # End of file
